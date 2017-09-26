94766cdbbf19958ca5f51cdbf3488d7e643126e4
Colin <colin@thinkr.fr>
2017-09-26 22:29:08 +0200

    devtools

--- data-raw/devtools_stuffs.R
+++ data-raw/devtools_stuffs.R
@@ -1,18 +1,37 @@
 # Devtools
 
-devtools::use_data_raw()
-devtools::use_code_of_conduct()
-devtools::use_mit_license()
-devtools::use_news_md()
-devtools::use_readme_rmd()
+library(devtools)
+
+# init stuffs
+use_code_of_conduct()
+use_mit_license()
+use_news_md()
+use_readme_rmd()
+use_testthat()
+use_travis()
+
+#init tests
+devtools::use_testthat()
+devtools::use_test("tidy_comb")
+devtools::use_test("tidy_string_dist")
+
+# package dependencies
+
 devtools::use_package("purrr")
 devtools::use_package("stringdist")
 devtools::use_package("rlang")
 devtools::use_package("tidyr")
-devtools::use_testthat()
-devtools::use_test("tidy_comb")
-devtools::use_test("tidy_string_dist")
+
+# Vignette
+
 devtools::use_vignette("Getting_started")
+build_vignettes()
+
+# run tests
+
+test()
+build_win()
+check()
 
 # Roxygen
 
